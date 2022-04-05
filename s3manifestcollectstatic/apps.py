b682a173c09812438a36e5e9c08d114a359d4992
PN-picsell <pierre-nicolas@picsellia.com>
2022-04-05 19:26:52 +0200

    0.0.1

--- s3manifestcollectstatic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PicselliaS3ManifestcollectstaticConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "picsellias3manifestcollectstatic"
