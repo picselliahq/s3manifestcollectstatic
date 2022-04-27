fc2b9458977f52c1c9d0f3aeba73f30518416e50
Thomas Darget <thomasdarget@hotmail.fr>
2022-04-27 12:02:25 +0200

    Test rename app

--- picselliacollectstatic/apps.py
+++ picselliacollectstatic/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class PicselliaCollectstaticConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "picsellia_collectstatic"
+    name = "picselliacollectstatic"
